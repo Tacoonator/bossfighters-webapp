fecab05293b2c0ce5fbff23780f5a80263a2e26e
Tacoonator <magnusbensmann@gmail.com>
2026-09-24 10:15:18 +0200

    Delete bossfighters-webapp-upload directory

--- bossfighters-webapp-upload/bosses/hellmo-crazby.js
+++
@@ -1,6 +0,0 @@
-window.BOSS_CONFIGS = window.BOSS_CONFIGS || {};
-window.BOSS_CONFIGS["hellmo-crazby"] = {
-  id: "hellmo-crazby",
-  name: "HELLMO & Crazby",
-  image: "assets/bosses/hellmo-crazby.png"
-};
