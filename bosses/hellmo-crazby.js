7b4ace9fff616390a60ac4a12374eb324e05415b
Tacoonator <magnusbensmann@gmail.com>
2026-09-24 10:15:44 +0200

    Add files via upload

---
+++ bosses/hellmo-crazby.js
@@ -0,0 +1,6 @@
+window.BOSS_CONFIGS = window.BOSS_CONFIGS || {};
+window.BOSS_CONFIGS["hellmo-crazby"] = {
+  id: "hellmo-crazby",
+  name: "HELLMO & Crazby",
+  image: "assets/bosses/hellmo-crazby.png"
+};
